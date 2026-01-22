44ded59002defe6308c814652724442f86a6c387
ehab <ehabrashedmassoud@gmail.com>
2026-01-22 13:29:02 +0200

    chore(all): ensure all recent changes are committed

--- web/app/dashboard/email-templates/page.tsx
+++ web/app/dashboard/email-templates/page.tsx
@@ -93,7 +93,7 @@ export default function EmailTemplatesPage() {
 
   return (
     <div
-      className={`flex-1 space-y-4 p-8 pt-6 ${isRtl ? "text-right" : ""}`}
+      className={`flex-1 space-y-4 p-4 md:p-8 pt-4 md:pt-6 ${isRtl ? "text-right" : ""}`}
       dir={isRtl ? "rtl" : "ltr"}
     >
       <div className="flex items-center justify-between">
--- web/app/dashboard/page.tsx
+++ web/app/dashboard/page.tsx
@@ -156,7 +156,7 @@ export default function DashboardPage() {
 
   return (
     <div
-      className={`flex-1 space-y-4 p-8 pt-6 ${isRtl ? "text-right" : ""}`}
+      className={`flex-1 space-y-4 p-4 md:p-8 pt-4 md:pt-6 ${isRtl ? "text-right" : ""}`}
       dir={isRtl ? "rtl" : "ltr"}
     >
       <div className={`flex items-center justify-between space-y-2 `}>
@@ -1231,7 +1231,7 @@ function TeacherDashboard({ stats }: { stats: TeacherStats }) {
 
   return (
     <div
-      className={`flex-1 space-y-4 p-8 pt-6 ${isRtl ? "text-right" : ""}`}
+      className={`flex-1 space-y-4 p-4 md:p-8 pt-4 md:pt-6 ${isRtl ? "text-right" : ""}`}
       dir={isRtl ? "rtl" : "ltr"}
     >
       <div className="flex items-center justify-between space-y-2">
